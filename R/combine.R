dffc5af48bc6afd3e3fc813a1e418b4584d31dd3
Will Tirone <will.tirone1@gmail.com>
2023-12-21 16:50:54 -0500

    adding helper func to calculate metrics

--- R/combine.R
+++ R/combine.R
@@ -1,3 +1,19 @@
+#' Helper function to compute metrics from a list of models summmaries.
+#'
+#' @param model_sum list of model summary objects
+#' @param metric desired metric to calculate
+#'
+#' @return A numeric vector
+#' @export
+#'
+#' @examples
+#' # internal use only
+metric_calc = function(model_sum, metric) {
+  # will want to add stuff to prevent invalid metrics being asked for
+  output = unlist(lapply(model_sum, function(lm_summary) lm_summary[[metric]]))
+  return(output)
+}
+
 #' A function to combine models. Making a change here to see the effect.
 #'
 #' @param ... list of models to summarize
@@ -13,8 +29,7 @@ smelt = function(...) {
 
   models = list(...)
   model_sum = lapply(models, summary)
-  output = unlist(lapply(model_sum,
-                         function(lm_summary) lm_summary$r.squared))
+  output = metric_calc(model_sum, 'r.squared')
 
   return(output)
 }
