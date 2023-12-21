ddf2d7e45eed783d08612c7eb94c4d9dead4b06b
Will Tirone <will.tirone1@gmail.com>
2023-12-20 19:02:20 -0500

    updating package

--- R/combine.R
+++ R/combine.R
@@ -1,32 +1,20 @@
-lm_1 = lm(mpg ~ disp + hp + drat + wt, data = mtcars)
-lm_2 = lm(mpg ~ disp + hp + drat, data = mtcars)
-lm_3 = lm(mpg ~ disp + hp, data = mtcars)
-
+#' A function to combine models
+#'
+#' @param ... list of models to summarize
+#'
+#' @return output
+#' @export
+#'
+#' @examples
+#' lm_1 = lm(mpg ~ cyl + disp + hp, data = mtcars)
+#' lm_2 = lm(mpg ~ cyl + wt + drat, data = mtcars)
+#' smelt(lm_1, lm_2)
 smelt = function(...) {
 
   models = list(...)
+  model_sum = lapply(models, summary)
+  output = unlist(lapply(model_sum,
+                         function(lm_summary) lm_summary$r.squared))
 
-  output = data.frame(
-
-  )
-
-  output = data.frame(model =
-                        c(as.character(t$call)[2],
-                          as.character(q$call)[2],
-                          as.character(z$call)[2]),
-                      R_squared =
-                        c(t$r.squared,
-                          q$r.squared,
-                          z$r.squared)
-  )
   return(output)
 }
-
-# feed in models
-check = list(lm_1, lm_2, lm_3)
-
-# apply summary
-#summaries = lapply(check, summary)
-#for (i in 1:length(summaries)) {
-#  print(summaries[[i]]$r.squared)
-#}
