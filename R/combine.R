e33c5ddaf498f55e7a4d40c5789cf29e2be58988
Will Tirone <will.tirone1@gmail.com>
2023-12-21 22:12:38 -0500

    working on proj

--- R/combine.R
+++ R/combine.R
@@ -9,14 +9,21 @@
 #' @examples
 #' # internal use only
 metric_calc = function(model_sum, metric) {
-  # will want to add stuff to prevent invalid metrics being asked for
-  output = unlist(lapply(model_sum, function(lm_summary) lm_summary[[metric]]))
+
+  if (metric %in% names(model_sum[[1]])){ # just checking ONE item for now
+    output = unlist(lapply(model_sum,
+                           function(lm_summary) lm_summary[[metric]]))
+  } else {
+    stop(paste("Metric '", metric, "' not found in summary object."))
+  }
+
   return(output)
 }
 
-#' A function to combine models. Making a change here to see the effect.
+#' A function to combine models.
 #'
-#' @param ... list of models to summarize
+#' @param ... list of models to summarize.
+#' @param metric metric to calculate.
 #'
 #' @return output
 #' @export
@@ -24,12 +31,12 @@ metric_calc = function(model_sum, metric) {
 #' @examples
 #' lm_1 = lm(mpg ~ cyl + disp + hp, data = mtcars)
 #' lm_2 = lm(mpg ~ cyl + wt + drat, data = mtcars)
-#' smelt(lm_1, lm_2)
-smelt = function(...) {
+#' smelt(lm_1, lm_2, metric = 'r.squared')
+smelt = function(..., metric) {
 
   models = list(...)
   model_sum = lapply(models, summary)
-  output = metric_calc(model_sum, 'r.squared')
+  output = metric_calc(model_sum, metric)
 
   return(output)
 }
